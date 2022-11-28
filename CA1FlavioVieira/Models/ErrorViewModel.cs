87cb9059a3b03749f76c984c0980330da0cc9d57
flavio <flaviongvieira@gmail.com>
2022-11-28 18:30:38 +0000

    Add project files.

---
+++ CA1FlavioVieira/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CA1FlavioVieira.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
